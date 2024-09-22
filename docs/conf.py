834aab267b8e43b401a8f3c48d6aa31dac6e01c7
huangc78 <chao-hui.huang@pfizer.com>
2024-09-22 16:42:06 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'qust'
+project = 'QuST'
 copyright = '2024, huangch'
 author = 'haungch'
 release = '0.1.0'
